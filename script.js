f84a4d26b21de9e49a0075b10e845328bb82943f
nikhil8851 <74211221+nikhil8851@users.noreply.github.com>
2024-09-21 12:32:08 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-// const codes = document.querySelectorAll('.code');
-//         codes.forEach((code, index) => {
-//             code.addEventListener('input', () => {
-//                 if (code.value.length === 1 && index < codes.length - 1) {
-//                     codes[index + 1].focus();
-//                 }
-//             });
-//             code.addEventListener('keydown', (e) => {
-//                 if (e.key === 'Backspace' && code.value.length === 0 && index > 0) {
-//                     codes[index - 1].focus();
-//                 }
-//             });
-//         });
- 
+const codes = document.querySelectorAll('.code');
+        codes.forEach((code, index) => {
+            code.addEventListener('input', () => {
+                if (code.value.length === 1 && index < codes.length - 1) {
+                    codes[index + 1].focus();
+                }
+            });
+            code.addEventListener('keydown', (e) => {
+                if (e.key === 'Backspace' && code.value.length === 0 && index > 0) {
+                    codes[index - 1].focus();
+                }
+            });
+        });
+() => { cy.visit(baseUrl + "/main.html"); 
+ () => { cy.visit(baseUrl + "/main.html");
+() => { cy.visit(baseUrl + "/main.html"); //always check this "visit" in your test case that it is set to - baseUrl + "/main.html" cy.get("#verification_heading").should("have.text", "Verify Your Account"); cy.get("#verification_subtext").should("exist"); cy.get(".code-container").find("input.code").eq(5).should("exist"); }
