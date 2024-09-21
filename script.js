bd669672044322722ca64992d70f489483ee8569
nikhil8851 <74211221+nikhil8851@users.noreply.github.com>
2024-09-21 12:27:52 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const codes = document.querySelectorAll('.code');
-        codes.forEach((code, index) => {
-            code.addEventListener('input', () => {
-                if (code.value.length === 1 && index < codes.length - 1) {
-                    codes[index + 1].focus();
-                }
-            });
-            code.addEventListener('keydown', (e) => {
-                if (e.key === 'Backspace' && code.value.length === 0 && index > 0) {
-                    codes[index - 1].focus();
-                }
-            });
-        });
+// const codes = document.querySelectorAll('.code');
+//         codes.forEach((code, index) => {
+//             code.addEventListener('input', () => {
+//                 if (code.value.length === 1 && index < codes.length - 1) {
+//                     codes[index + 1].focus();
+//                 }
+//             });
+//             code.addEventListener('keydown', (e) => {
+//                 if (e.key === 'Backspace' && code.value.length === 0 && index > 0) {
+//                     codes[index - 1].focus();
+//                 }
+//             });
+//         });
  
