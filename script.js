c03ca06120d205744e334176048df3a8a982ddc3
nikhil8851 <74211221+nikhil8851@users.noreply.github.com>
2024-09-21 12:11:40 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-
 const codes = document.querySelectorAll('.code');
-
-codes.forEach((code, index) => {
-    code.addEventListener('keydown', (e) => {
-        if (e.key === 'Backspace' && index !== 0) {
-            codes[index - 1].focus();
-        }
-    });
-
-    code.addEventListener('input', () => {
-        if (code.value.length === 1 && index < codes.length - 1) {
-            codes[index + 1].focus();
-        }
-    });
-});
+        codes.forEach((code, index) => {
+            code.addEventListener('input', () => {
+                if (code.value.length === 1 && index < codes.length - 1) {
+                    codes[index + 1].focus();
+                }
+            });
+            code.addEventListener('keydown', (e) => {
+                if (e.key === 'Backspace' && code.value.length === 0 && index > 0) {
+                    codes[index - 1].focus();
+                }
+            });
+        });
+ 
